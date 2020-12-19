aa3904f134929d2c26b42971c45eaa9bc3242918
martinmazas <mazasmartin@gmail.com>
2020-12-19 17:55:31 +0200

    fixing date insert

--- src/costManager/model/Demo.java
+++ src/costManager/model/Demo.java
@@ -12,17 +12,16 @@ public class Demo {
 //            model.deleteTable("categories");
 //            model.createTable("categories", "(id int generated always as identity PRIMARY KEY , name varchar(256))");
 //            model.createTable("inventory", "(id int generated always as identity PRIMARY KEY,categoryId int,amount double, " +
-//                    "currency varchar(250),description varchar(250),date varchar(256), FOREIGN KEY (categoryId) REFERENCES categories(id))");
+//                    "currency varchar(250),description varchar(250),date DATE, FOREIGN KEY (categoryId) REFERENCES categories(id))");
 
 
-
-            Category cat = new Category(4,"Shopping");
-            model.addNewCategory(cat);
+//            Category cat = new Category(4,"Shopping");
+//            model.addNewCategory(cat);
             List<Category> categoryList = model.getCategoryList();
-            for(int i = 0; i<categoryList.size(); i++) {
-                System.out.println(categoryList.get(i));
-            }
-            CostItem item = new CostItem(15, categoryList.get(0),14.33, Currency.EURO,"test4","20-12-2020");
+//            for(int i = 0; i<categoryList.size(); i++) {
+//                System.out.println(categoryList.get(i));
+//            }
+            CostItem item = new CostItem(15, categoryList.get(0),14.33, Currency.EURO,"test4","2020-12-03");
             model.addCostItem(item);
 
         }catch (CostManagerException e) {
