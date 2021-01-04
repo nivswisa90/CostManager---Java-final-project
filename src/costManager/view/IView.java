b9eb459e0fb8bca28b0bf62ecf3dbe8b8e0242c4
martinmazas <mazasmartin@gmail.com>
2021-01-04 08:43:25 +0200

    starting to work on responsive view

--- src/costManager/view/IView.java
+++ src/costManager/view/IView.java
@@ -3,5 +3,8 @@ package costManager.view;
 import java.util.Map;
 
 public interface IView {
-    public void displayPieChart(Map map);
+    public void displayAddCostItem();
+    public void displayAddNewCategory();
+    public void displayGetCostReport();
+    //    public void displayPieChart(Map map);
 }
