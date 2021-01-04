ad97c4f13922759fd2d58d2a3cb9044352d9d3e3
martinmazas <mazasmartin@gmail.com>
2021-01-04 13:31:13 +0200

    adding comments and layout need modifications

--- src/costManager/view/ViewCostItem.java
+++ src/costManager/view/ViewCostItem.java
@@ -4,14 +4,21 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ViewCostItem {
-    //add a new cost members
-    private JPanel addCostPanel;
+    /**
+     * view cost item members
+     */
+    private JPanel addCostPanel, submitPanel;
     private JLabel categoryLabel, sumLabel, currencyLabel, dateLabel;
     private JTextField categoryField, sumField, currencyField, dateField;
-    private JButton submitCostButton;
-    
+    private JButton submitCostButton, backButton;
+
+    /**
+     * view cost item constructor
+     */
+
     public ViewCostItem() {
         addCostPanel = new JPanel();
+        submitPanel = new JPanel();
         categoryLabel = new JLabel("Choose category");
         sumLabel = new JLabel("Insert the amount");
         currencyLabel = new JLabel("Choose the currency");
@@ -21,8 +28,13 @@ public class ViewCostItem {
         currencyField = new JTextField();
         dateField = new JTextField();
         submitCostButton = new JButton("Submit");
+        backButton = new JButton("Back");
     }
-    
+
+    /**
+     * Prepares the add new cost panel
+     * @return the cost item panel
+     */
     public JPanel addFieldsToPanel() {
         addCostPanel.setBackground(Color.YELLOW);
         addCostPanel.setLayout(new GridLayout(5, 2));
@@ -35,10 +47,11 @@ public class ViewCostItem {
         addCostPanel.add(currencyField);
         addCostPanel.add(dateLabel);
         addCostPanel.add(dateField);
+        addCostPanel.add(backButton);
         addCostPanel.add(submitCostButton);
         
         return addCostPanel;
-    } 
+    }
     
     
 }
--- src/costManager/view/ViewNewCategory.java
+++ src/costManager/view/ViewNewCategory.java
@@ -3,29 +3,50 @@ package costManager.view;
 import javax.swing.*;
 import java.awt.*;
 
+
 public class ViewNewCategory {
-    //add new category members
+    /**
+     * view new category members
+     */
     private JPanel addCategoryPanel;
     private JLabel addCategoryLabel;
     private JTextField addCategoryField;
-    private JButton addCategoryButton;
-    
+    private JButton addCategoryButton, backButton;
+
+    /**
+     * view new catergory constructor
+     */
     public ViewNewCategory() {
-        //add category Panel
         addCategoryPanel = new JPanel();
         addCategoryLabel = new JLabel("Insert a new category");
         addCategoryField = new JTextField();
         addCategoryButton = new JButton("Add");
+        backButton = new JButton("Back");
     }
-    
+
+    /**
+     * Prepares the new category panel
+     * @return the new category panel
+     */
     public JPanel addToCategoryPanel() {
-        addCategoryPanel.setBackground(Color.GRAY);
-        addCategoryPanel.setLayout(new GridLayout(2,1));
+        addCategoryPanel.setBackground(Color.red);
+        addCategoryPanel.setLayout(new GridLayout(2,2));
 
         addCategoryPanel.add(addCategoryLabel);
         addCategoryPanel.add(addCategoryField);
+        addCategoryPanel.add(backButton);
         addCategoryPanel.add(addCategoryButton);
-        
+
+//        backButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+////                addCategoryPanel.setVisible(false);
+//                View view = new View();
+//                view.init();
+//            }
+//        });
+
         return addCategoryPanel;
     }
+
 }
