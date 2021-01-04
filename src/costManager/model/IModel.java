0fd32a1e178562d15ebe18f95e539b53d4efcc2c
martinmazas <mazasmartin@gmail.com>
2021-01-04 07:58:10 +0200

    comments in IModel

--- src/costManager/model/IModel.java
+++ src/costManager/model/IModel.java
@@ -1,12 +1,30 @@
 package costManager.model;
 
-import java.util.Date;
 
 public interface IModel {
+    /**
+     * add a new cost to inventory table in database
+     */
     public void addCostItem(CostItem item) throws CostManagerException;
+
+    /**
+     * delete a cost from inventory table in database
+     */
     public void deleteCostItem(CostItem item) throws CostManagerException;
+
+    /**
+     * add a new cost to categories table in database
+     */
     public void addNewCategory(Category category) throws CostManagerException;
+
+    /**
+     * get report from all the costs between specific dates
+     */
     public void getCostReport(String start, String end) throws CostManagerException;
+
+    /**
+     * get pie chartr from all the costs between specific dates
+     */
     public void getPieChart(String start, String end) throws CostManagerException;// and the way it splits?????
 
 }
